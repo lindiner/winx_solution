875f13b011c6bbdb745d057426d07b95eb356cfc
lindiner <51235468+lindiner@users.noreply.github.com>
2020-11-17 21:26:46 -0300

    inicio da modelagem do banco de dados

--- winx_sistema/winx_sistema/settings.py
+++ winx_sistema/winx_sistema/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'winx_sistema.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'winx_sistema',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- winx_sistema/winx_sistema_loja/models.py
+++ winx_sistema/winx_sistema_loja/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class vendedor(models.Model):
+    idpessoa = models.AutoField(primary_key=True)
+    codVendedor = models.models.IPAddressField(_(""))
+    name = models.CharField(max_length=128)
+    email = models.EmailField(_(""), max_length=254)
+    idade = models.models.DateField(_(""), auto_now=False, auto_now_add=False)
+    cpf = models.BinaryField(_(""))
+    cnpj = models.BigAutoField(_(""))
+    cep = models.models.BigAutoField(_(""))
+    endereco = models.CharField(_(""), max_length=50)
+    bairro = models.CharField(_(""), max_length=50)
+    cidade = models.CharField(_(""), max_length=50)
+    estado = models.CharField(_(""), max_length=50)
+
+
+class login(models.Model):
+    usuario = models.CharField(max_length=50)
+    senha = models.models.TextField(_(""))
+    vendedor = models.ManyToManyField(vendedor.idpessoa)
+
+
+class Produto(models.Model):
+    STATUS = (
+        ('active', 'Ativo'),
+        ('draft', 'Inativo')
+        )
+
+    status = models.CharField(max_length=15, choices=STATUS)
