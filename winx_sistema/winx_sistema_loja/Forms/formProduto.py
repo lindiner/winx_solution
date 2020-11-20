1e9753eaaf66d7dcd8dc597bca85b025a4114992
carolesj <carolinejesuino.ns@gmail.com>
2020-11-20 16:20:52 -0300

    Remove unused folder

--- winx_sistema/winx_sistema_loja/Forms/formProduto.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from.models import produto
-
-class produtoForm(ModelForm):
-    class Meta:
-        model = produto
-        fields = ['idProduto','name','precoProduto','codigoProduto','quantidadeEstoque','corProduto','tamanho']
-        
